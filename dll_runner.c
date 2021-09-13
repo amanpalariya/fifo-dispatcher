82ce26580ac5dd72df3cd6841abab67a52781d34
Aman Palariya <2019csb1068@iitrpr.ac.in>
2021-09-13 19:57:14 +0530

    Write starter code for DLL opening and running

--- dll_runner.c
+++ dll_runner.c
@@ -1,5 +1,22 @@
 #include "dll_runner.h"
 
-#include "ds.h"
+#include <dlfcn.h>
 
-void handle_request(struct request* req) {}
+#include "ds.h"
+#include "logger.h"
+
+void handle_request(struct request* req) {
+    void* handle = dlopen(req->dll_name, RTLD_LAZY);
+    if (!handle) {
+        log_error(dlerror());
+        return;
+    }
+    void (*func)(char** args) = dlsym(handle, req->func_name);
+    char* error = dlerror();
+    if (error) {
+        log_error(error);
+        return;
+    }
+    (*func)(req->func_args);
+    dlclose(handle);
+}
